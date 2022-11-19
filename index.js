c7ddf6febeb2bd9f0e0916de2fb9311100b5e0e2
wildstreams <staging.wildstreams@gmail.com@>
2022-11-19 10:10:43 +0000

    setup streaming and voting

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ client.on('connected', () => {
     client.say(channel, `${username} has connected!`);
 });
 
+var player = require('play-sound')(opts = {})
+
 client.on('message', (channel, user, message, self) => {
     if(self) return;
 
@@ -30,5 +32,35 @@ client.on('message', (channel, user, message, self) => {
 
     if(message == '!roll') {
         client.say(channel, `@${user.username} rolled a ${Math.floor(Math.random() * 6) + 1}!`);
+    }
+
+    if(message == "!moo") {
+        client.say(channel, `@${user.username}, wants to moo!`);
+        player.play('C:/Users/sam/wildstreams/moo.mp3', function(err){
+            if (err) console.log(err)
+        })
+    }
+
+    if(message == "!oink") {
+        client.say(channel, `@${user.username}, wants to oik!`);
+        player.play('C:/Users/sam/wildstreams/oink.mp3', function(err){
+            if (err) console.log(err)
+        })
+    }
+
+    if(message == "!woof") {
+        client.say(channel, `@${user.username}, wants to woof!`);
+        player.play('C:/Users/sam/wildstreams/woof.mp3', function(err){
+            if (err) console.log(err)
+        })
+    }
+
+    if(message == "!meow") {
+        client.say(channel, `@${user.username}, wants to meow!`);
+        player.play('C:/Users/sam/wildstreams/meow.mp3', function(err, stdout, stderr) {
+            if (err) console.log(err)
+            console.log(stdout)                                                                                               
+            console.log(stderr)
+         });
     }
 });
